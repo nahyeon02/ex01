131bc627d9cf678d8fcb4941300dcf69a55fe245
최나현 로컬 <ssdx02@gmail.com>
2020-01-20 01:19:03 +0900

    part2, ~p.126 기본세팅과 controller

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,11 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*")
+//현재 클래스의 모든 메서드들의 기본적인 URL
+public class SampleController {
+
+}
